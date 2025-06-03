638639efa3ca2e84e876053cb9fffe797bfef57d
Jay <patiljay956@gmail.com>
2025-06-03 23:45:02 +0530

    feat: implement user management with getUserById and updateUserDetails functions, and add role field to user model

---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,67 @@
+import { User } from "../models/user.models.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiErrors.js";
+import { UserRolesEnum } from "../utils/constants.js";
+
+const getUserById = asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+
+    if (!userId) {
+        throw new ApiError(400, "User ID is required");
+    }
+
+    const user = await User.findById(userId)
+        .select(
+            "-password -refreshToken -isEmailVerified -emailVerificationToken -emailVerificationExpiry -forgotPasswordToken -forgotPasswordExpiry -__v -role",
+        )
+        .lean();
+
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, user, "User fetched successfully"));
+});
+
+const updateUserDetails = asyncHandler(async (req, res) => {
+    const user = req.user; // Get the authenticated user from the request
+
+    if (!user) throw new ApiError(401, "User not authenticated");
+
+    const { name, username } = req.body;
+
+    // Validate the input
+    if (!name || !username) {
+        throw new ApiError(400, "Name and username are required");
+    }
+
+    // Update the user details
+    const updatedUser = await User.findByIdAndUpdate(
+        user._id,
+        { name, username },
+        { new: true, runValidators: true },
+    )
+        .select(
+            "-password -refreshToken -isEmailVerified -emailVerificationToken -emailVerificationExpiry -forgotPasswordToken -forgotPasswordExpiry -__v",
+        )
+        .lean();
+
+    if (!updatedUser) {
+        throw new ApiError(404, "User not found");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                updatedUser,
+                "User details updated successfully",
+            ),
+        );
+});
+
+export { getUserById, updateUserDetails };
--- Backend/src/models/user.models.js
+++ Backend/src/models/user.models.js
@@ -55,6 +55,11 @@ const userSchema = new mongoose.Schema(
         refreshToken: {
             type: String,
         },
+        role: {
+            type: String,
+            enum: ["user", "admin"],
+            default: "user",
+        },
     },
     { timestamps: true },
 );
